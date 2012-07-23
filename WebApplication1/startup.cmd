79a4ef66b7e541d500c363aa6e96c43c9e7ed1dc
Simon Mansfield <siyfion@gmail.com>
2012-07-23 13:59:51 +0100

    Added scripts needed to install Inkscape when published

--- WebApplication1/startup.cmd
+++
@@ -1,3 +0,0 @@
-cd "%~dp0"
-curl -O http://switch.dl.sourceforge.net/project/inkscape/inkscape/0.48.2/Inkscape-0.48.2-1-win32.exe
-Inkscape-0.48.2-1-win32.exe /S
---
+++ WebApplication1/startup/startup.cmd
@@ -0,0 +1,13 @@
+﻿@REM Setting up Azure Stroage Credentials
+set azurestoragename=planglow
+set azurestoragekey=8ColKNpn1RRMDRUwQniowKazfhmoGBJSwVlR6zks0up2Kcg/WkNH4dsKoVC1C6/FEPJcZqTP/+EAlSln2SxKPA==
+set storagecontainername=vivcontainer
+
+@REM Download Inkscape Installer
+set filename=inkscape.exe
+packagedownloader.exe "%azurestoragename%" "%azurestoragekey%" "%storagecontainername%" "%filename%"
+
+@REM Install Inkscape
+inkscape.exe /S
+exit /b 0
+
